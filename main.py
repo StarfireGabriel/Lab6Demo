9dddbcd4379157f7e31d9893667793cf3e797fa8
StarfireGabriel <sirstarfire@gmail.com>
2024-10-23 22:30:18 -0400

    Decoder updated

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-def add(x,y):
-    return x + y
-
-def subtract(x,y):
-    return x - y
+def decode(encoded_password):
+    decoded = ""
+    for char in encoded_password:
+        temp = int(char) - 3
+        if temp < 0:
+            temp += 10
+        decoded += str(temp)
+    
+    return decoded
 
 if __name__ == "__main__":
-    print(add(1,2))
+    while True:
+        print("     Menu     ")
+        print("1. Decode password")
+        print("2. Exit")
+        choice = input("\nEnter menu option: ")
+
+        if choice == "1":
+            user_input = input("Enter encoded password: ")
+            decoded_password = decode(user_input)
+            print(f"Decoded password is: {decoded_password}")
+        
+        elif choice == "2":
+            break
+        
+        else:
+            print("Not a valid menu option")
